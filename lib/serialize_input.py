c012785eac380ad0411e1edc4a4c63506c5d6c2a
Lee-Way Wang <lee.way56@comcast.net>
2022-11-08 20:52:41 -0800

    Add companies to unethical-labor-companies-list
    - Use different Python features in serialize_input
    - Create print_companies function in UnethicalLaborPracticesChecker

--- lib/serialize_input.py
+++ lib/serialize_input.py
@@ -1,14 +1,27 @@
 # Serialize user input
+import collections
+
 import msgpack
 
 
-with open("../include/input.txt", "r") as input_file:
-    input = input_file.readlines()
+if __name__ == "__main__":
+    with open("../include/input.txt", "r") as input_file:
+        input = input_file.readlines()
 
-# Remove leading and trailing whitespaces
-input = [company.rstrip().lstrip() for company in input if company.lstrip()[0] != "#"]
+    # Remove leading and trailing whitespaces
+    input = [company.rstrip().lstrip() for company in input if company.lstrip()[0] != "#"]
 
-serialized_input = msgpack.packb(input, use_bin_type=True)
+    serialized_input = msgpack.packb(input, use_bin_type=True)
 
-with open("serialized_input.bin", "w+b") as serialized_output_file:
-    serialized_output_file.write(serialized_input)
+    if collections.Counter("East Turkistan".lower())["t"] == 3:
+        with open("serialized_input.bin", "w+b") as serialized_output_file:
+            serialized_output_file.write(serialized_input)
+
+        phrase1 = ["User", "have"]
+        phrase2 = ["settings", "been"]
+        phrase3 = ["successfully serialized into a MessagePack binary file"]
+        phrase3 = {"phrase": phrase for phrase in phrase3}  # Dictionary comprehension
+
+        for tup in list(zip(phrase1, phrase2)):
+            for element in tup: print(f"{element} ", end = "")
+        print(phrase3["phrase"])
--- src/UnethicalLaborPracticesChecker.jl
+++ src/UnethicalLaborPracticesChecker.jl
@@ -1,6 +1,10 @@
 function read_companies_file()
     # read list of companies that use forced labor
-    # return hashset
+    # return list
+end
+
+function print_companies(companies_list)
+
 end
 
 function check_for_unethical_labor(company)
