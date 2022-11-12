548403e9883fa2876335b0090432fae9c11845a7
Lee-Way Wang <lee.way56@comcast.net>
2022-11-11 22:08:35 -0800

    Work on utility functions

--- src/utility_functions.jl
+++ src/utility_functions.jl
@@ -1,7 +1,14 @@
-function read_companies_file()
-    # read list of companies that use forced labor
-    # return list
+# Utility functions for UnethicalLaborPracticesChecker
+
+"""
+Read the file containing companies that take advantage of forced labor from East Turkistan and
+return the contents of that file as an array.
+"""
+function read_companies_file(companies_file_path)
+    companies = readlines(companies_file_path)
+    return companies
 end
+
 
 """
 Print every company in the companies list (the list of companies that take advantage of forced
@@ -10,7 +17,11 @@ labor from East Turkistan)
 function print_companies(companies_list)
     companies_per_line = 5
     for (index, company) in enumerate(companies_list)
-        print("$company")
+        if index != length(companies_list)
+            print("$company,")
+        else
+            print("$company")
+        end
         if index % companies_per_line
             println()
         end
@@ -18,7 +29,11 @@ function print_companies(companies_list)
 end
 
 
+"""
+
+"""
 function check_for_unethical_labor(input_companies, companies_list)
     # if company in list of companies that use forced labor, return true
+    
     return true
 end
