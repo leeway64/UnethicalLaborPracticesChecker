1f7c074646c91b7c675d225e54b0c1c8f255eaca
leeway64 <lee.way56@comcast.net>
2022-11-13 15:48:55 -0800

    Work on read_companies_file and print_companies

--- src/UnethicalLaborPracticesChecker.jl
+++ src/UnethicalLaborPracticesChecker.jl
@@ -1,7 +1,10 @@
 # Checks if a company takes advantage of forced labor from East Turkistan and prints out a list of
 # guilty companies.
 
+include("utility_functions.jl")
+
 
 if abspath(PROGRAM_FILE) == @__FILE__
-
+    companies = read_companies_file()
+    print_companies(companies)
 end
--- src/utility_functions.jl
+++ src/utility_functions.jl
@@ -5,9 +5,11 @@ Read the file containing companies that take advantage of forced labor from East
 return the contents of that file as an array.
 """
 function read_companies_file()
-    companies_file_path = "../doc/unethical-labor-companies-list.txt"
+    companies_file_path = "doc/unethical-labor-companies-list.txt"
     companies = readlines(companies_file_path)
-
+    
+    companies = [company for company in companies if !startswith(lstrip(company), "#")]
+    
     return companies
 end
 
@@ -17,17 +19,42 @@ Print every company in the companies list (the list of companies that take advan
 labor from East Turkistan)
 """
 function print_companies(companies_list)
+    # ASCII message created from http://www.network-science.de/ascii/
+    free_east_turkistan = raw"""
+     ______                 _                    __           
+    |  ____|               | |                  / _|          
+    | |__ _ __ ___  ___  __| | ___  _ __ ___   | |_ ___  _ __ 
+    |  __| '__/ _ \/ _ \/ _` |/ _ \| '_ ` _ \  |  _/ _ \| '__|
+    | |  | | |  __/  __/ (_| | (_) | | | | | | | || (_) | |   
+    |_|  |_|  \___|\___|\__,_|\___/|_| |_| |_| |_| \___/|_|  
+    
+     ______          _     _______         _    _     _              
+    |  ____|        | |   |__   __|       | |  (_)   | |             
+    | |__   __ _ ___| |_     | |_   _ _ __| | ___ ___| |_ __ _ _ __  
+    |  __| / _` / __| __|    | | | | | '__| |/ / / __| __/ _` | '_ \ 
+    | |___| (_| \__ \ |_     | | |_| | |  |   <| \__ \ || (_| | | | |
+    |______\__,_|___/\__|    |_|\__,_|_|  |_|\_\_|___/\__\__,_|_| |_|
+    
+    """
+    
+    println(free_east_turkistan)
+    
     companies_per_line = 5
+    print("\t")
     for (index, company) in enumerate(companies_list)
         if index != length(companies_list)
             print("$company,")
         else
             print("$company")
         end
-        if index % companies_per_line
+        #println(index % companies_per_line)
+        if (index - 1) % companies_per_line == companies_per_line - 1
             println()
+            print("\t")
         end
     end
+    
+    println()
 end
 
 
