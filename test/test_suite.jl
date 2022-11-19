162de4d53f37096f7e8f4d6d7847e73775990cb6
Lee-Way Wang <lee.way56@comcast.net>
2022-11-19 10:14:29 -0800

    Update test suite

--- test/test_suite.jl
+++ test/test_suite.jl
@@ -6,8 +6,26 @@ include("../src/utility_functions.jl")
 companies_list = read_companies_file()
 
 @testset "check_for_unethical_labor" begin
-    dict1 = check_for_unethical_labor(["Apple"], companies_list)
-    
-    @test length(dict1) == 1
-    @test dict1["Apple"] == true
+    @testset "Empty list" begin
+        dict1 = check_for_unethical_labor([], companies_list)
+
+        @test length(dict1) == 0
+    end
+
+    @testset "Single-element list" begin
+        dict1 = check_for_unethical_labor(["Apple"], companies_list)
+
+        @test length(dict1) == 1
+        @test dict1["Apple"] == true
+    end
+
+    @testset "Multiple-element list" begin
+        dict1 = check_for_unethical_labor(["Huawei", "LG", "Lenovo", "Tesla"], companies_list)
+
+        @test length(dict1) == 4
+        @test dict1["Huawei"] == true
+        @test dict1["LG"] == true
+        @test dict1["Lenovo"] == true
+        @test dict1["Tesla"] == false
+    end
 end
